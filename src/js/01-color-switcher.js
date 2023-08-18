d2e9cde2b6f84ea8c21fffbb27f43a37a407c0ee
artm22es <artm22es@gmail.com>
2023-08-18 14:23:47 +0200

    some-commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+const body = document.querySelector('body')
 
+startBtn.addEventListener('click', handlerStart)
+stopBtn.addEventListener('click', handlerStop)
+
+
+function handlerStart() {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true
+    stopBtn.disabled = false
+
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+}
+
+function handlerStop() {
+    startBtn.disabled = false
+    stopBtn.disabled = true
+
+    clearInterval(timerId)
+}
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
